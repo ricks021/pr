1320ac90f570ba2fe18e2262b35751476d403941
ricks021 <ricky.sch021@gmail.com>
2022-02-20 17:22:22 -0500

    git add.

--- profile-two/src/App.js
+++ profile-two/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
 import Home from './Home/Home';
 import Name from './Name/Name';
 import Username from "./Username/Username";
@@ -13,34 +13,34 @@ const App = () =>{
     <Router>
       <main>
         
-        <Route path="/" exact>
-          <Home/>
-        </Route>
+          <Route path="/" exact>
+            <Home/>
+          </Route>
 
-        <Route path="/name" exact>
-          <Name/>
-        </Route>
+          <Route path="/name" exact>
+            <Name/>
+          </Route>
 
-        <Route path="/username" exact>
-          <Username/>
-        </Route>
+          <Route path="/username" exact>
+            <Username/>
+          </Route>
 
-        <Route path="/password" exact>
-          <Password/>
-        </Route>
+          <Route path="/password" exact>
+            <Password/>
+          </Route>
 
-        <Route path="/address" exact>
-          <Address/>
-        </Route>
+          <Route path="/address" exact>
+            <Address/>
+          </Route>
 
-        <Route path="/email" exact>
-          <Email/>
-        </Route>
+          <Route path="/email" exact>
+            <Email/>
+          </Route>
 
-        <Route path="/deleteAcc" exact>
-          <DeleteAcc/>
-        </Route>
-
+          <Route path="/deleteAcc" exact>
+            <DeleteAcc/>
+          </Route>
+        
       </main>
     </Router>
   );
